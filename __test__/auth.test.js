f6ac9a4b34f7387a2e6f21db5eceed38a3969155
NashatAlzaatreh <nashat.alzaatra@yahoo.com>
2021-11-26 13:41:27 +0200

    test6

--- __test__/auth.test.js
+++ __test__/auth.test.js
@@ -6,58 +6,45 @@ const supertest = require("supertest");
 const { app } = require("../src/server");
 const mockReq = supertest(app);
 const faker = require("faker");
-const { db } = require("../src/models/index");
+// const { db } = require("../src/models/index");
 
-// beforeAll((done) => {
-//   done();
-// });
-
-afterAll((done) => {
-  db.close();
-  done();
-});
+let users = {
+  admin: { username: faker.name.findName(), password: "test", role: "admin" },
+  // editor: { username: faker.name.findName(), password: "test", role: "editor" },
+  // user: { username: faker.name.findName(), password: "test", role: "user" },
+};
 
 describe("sign-up sign-in", () => {
-  const newUser = {
-    username: faker.name.findName(),
-    password: faker.name.findName(),
-    Email: faker.name.findName(),
-    role: "admin",
-  };
-  it("sign up", async () => {
-    const res = await mockReq.post("/signup").send(newUser);
-    expect(res.status).toEqual(201);
-    expect(res.body.token).toBeDefined();
-    expect(res.body.user.username).toEqual(newUser.username);
-  });
+  Object.keys(users).forEach((user) => {
+    it("sign up", async () => {
+      const res = await mockReq.post("/signup").send(users[user]);
+      expect(res.status).toEqual(201);
+      expect(res.body.token).toBeDefined();
+      expect(res.body.user.username).toEqual(users[user].username);
+    });
 
-  it("sign in", async () => {
-    const res = await mockReq
-      .post("/signin")
-      .auth(newUser.username, newUser.password);
-    expect(res.status).toEqual(200);
-    expect(res.body.token).toBeDefined();
-    expect(res.body.user.username).toEqual(newUser.username);
+    it("sign in", async () => {
+      const res = await mockReq
+        .post("/signin")
+        .auth(users[user].username, users[user].password);
+      expect(res.status).toEqual(200);
+      expect(res.body.token).toBeDefined();
+      expect(res.body.user.username).toEqual(users[user].username);
+    });
   });
 });
 
 let token;
 
-// describe("/users", () => {
-//   const newUser = {
-//     username: faker.name.findName(),
-//     password: faker.name.findName(),
-//     Email: faker.name.findName(),
-//     role: "admin",
-//   };
-//   it("/users", async () => {
-//     const res1 = await mockReq
-//       .post("/signin")
-//       .auth(newUser.username, newUser.password);
-//     token = res1.body.token;
-//     const res = await mockReq
-//       .get("/users")
-//       .set({ Authorization: `Bearer ${token}` });
-//     expect(res.status).toEqual(200);
-//   });
-// });
+describe("/users", () => {
+  it("/users", async () => {
+    const res1 = await mockReq
+      .post("/signin")
+      .auth(users.admin.username, users.admin.password);
+    token = res1.body.token;
+    const res = await mockReq
+      .get("/users")
+      .set({ Authorization: `Bearer ${token}` });
+    expect(res.status).toEqual(200);
+  });
+});
